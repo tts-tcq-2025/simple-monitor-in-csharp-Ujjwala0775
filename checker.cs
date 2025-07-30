ceb73495c0c22d489e9ff5a300a24bc3e0bde539
Ujjwala <43949386+Ujjwala0775@users.noreply.github.com>
2025-07-30 17:41:42 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -8,10 +8,12 @@ class Checker
         if(temperature < 0 || temperature > 45) {
             Console.WriteLine("Temperature is out of range!");
             return false;
-        } else if(soc < 20 || soc > 80) {
+        } 
+        if(soc < 20 || soc > 80) {
             Console.WriteLine("State of Charge is out of range!");
             return false;
-        } else if(chargeRate > 0.8) {
+        } 
+        if(chargeRate > 0.8) {
             Console.WriteLine("Charge Rate is out of range!");
             return false;
         }
